3965a8646fe12b8087359ab6c772eb1c4ec2e610
Tom Chestnut <tchestnut85@gmail.com>
2021-02-27 16:29:24 -0500

    Created user routes to get user's thoughts and create a new thought and connected those routes to DynamoDB.

--- server/db/LoadThoughts.js
+++ server/db/LoadThoughts.js
@@ -1,29 +1,30 @@
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk');
 const fs = require('fs');
 
 AWS.config.update({
-  region: "us-east-2",
-  endpoint: "http://localhost:8000"
+  region: 'us-east-2',
+  endpoint: 'http://localhost:8000'
 });
-const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+const dynamodb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 
-console.log("Importing thoughts into DynamoDB. Please wait.");
+console.log('Importing thoughts into DynamoDB. Please wait.');
 const allUsers = JSON.parse(fs.readFileSync('./server/seed/users.json', 'utf8'));
+
 allUsers.forEach(user => {
   const params = {
-    TableName: "Thoughts",
+    TableName: 'Thoughts',
     Item: {
-      "username": user.username,
-      "createdAt": user.createdAt,
-      "thought": user.thought
+      'username': user.username,
+      'createdAt': user.createdAt,
+      'thought': user.thought
     }
   };
 
   dynamodb.put(params, (err, data) => {
     if (err) {
-      console.error("Unable to add thought", user.username, ". Error JSON:", JSON.stringify(err, null, 2));
+      console.error('Unable to add thought', user.username, '. Error JSON:', JSON.stringify(err, null, 2));
     } else {
-      console.log("PutItem succeeded:", user.username);
+      console.log('PutItem succeeded:', user.username);
     }
   });
 });
--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,157 +1,80 @@
 const express = require('express');
 const router = express.Router();
-const AWS = require("aws-sdk");
-const awsConfig = {
-  region: "us-east-2",
-  endpoint: "http://localhost:8000",
 
+const AWS = require('aws-sdk');
+const awsConfig = {
+  region: 'us-east-2',
+  endpoint: 'http://localhost:8000'
 };
 AWS.config.update(awsConfig);
 const dynamodb = new AWS.DynamoDB.DocumentClient();
-const table = "Thoughts";
+const table = 'Thoughts';
 
-// get all users' thoughts
+// Route to get all users and their thoughts
 router.get('/users', (req, res) => {
   const params = {
     TableName: table
   };
+  // scan to return all items in the table
   dynamodb.scan(params, (err, data) => {
     if (err) {
-      res.status(500).json(err); // an error occurred
+      res.status(500).json(err);
     } else {
-      res.json(data.Items)
+      res.json(data.Items);
     }
   });
-})
+});
 
-// get thoughts from a user
+// Route to get all thoughts from a single user
 router.get('/users/:username', (req, res) => {
-  console.log(`Querying for thought(s) from ${req.params.username}.`);
+  console.log(`Querying for thoughts from ${req.params.username}.`);
+
   const params = {
     TableName: table,
-    KeyConditionExpression: "#un = :user",
+    KeyConditionExpression: '#un = :user',
     ExpressionAttributeNames: {
-      "#un": "username",
-      "#ca": "createdAt",
-      "#th": "thought"
+      '#un': 'username',
+      '#ca': 'createdAt',
+      '#th': 'thought'
     },
     ExpressionAttributeValues: {
-      ":user": req.params.username
+      ':user': req.params.username
     },
-    ProjectionExpression: "#th, #ca"
+    ProjectionExpression: '#th, #ca',
+    ScanIndexForward: false
   };
 
   dynamodb.query(params, (err, data) => {
     if (err) {
-      console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-      res.status(500).json(err); // an error occurred
+      console.error('Unable to query. Error:', JSON.stringify(err, null, 2));
+      res.status(500).json(err);
     } else {
-      console.log("Query succeeded.");
-      res.json(data.Items)
+      console.log('Query succeeded.');
+      res.json(data.Items);
     }
   });
 });
 
-// Create new user
+// Route to create a thought by a user
 router.post('/users', (req, res) => {
   const params = {
     TableName: table,
     Item: {
-      "username": req.body.username,
-      "createdAt": Date.now(),
-      "thought": req.body.thought
+      'username': req.body.username,
+      'createdAt': Date.now(),
+      'thought': req.body.thought
     }
   };
+
   dynamodb.put(params, (err, data) => {
     if (err) {
-      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-      res.status(500).json(err); // an error occurred
+      console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
+      res.status(500).json(err);
     } else {
-      console.log("Added item:", JSON.stringify(data));
-      res.json({ "Added": JSON.stringify(data, null, 2) });
-    }
-  });
-});
-// // Create new user
-// router.get('/create', (req, res) => {
-//   const params = {
-//     TableName: table,
-//     Item: {
-//       "username": "Carol Dweck",
-//       "createdAt": 1602018401105,
-//       "thought": "You can suffer the pain of change or suffer remaining the way you are."
-//     }
-//   };
-//   // const params = {
-//   //   TableName: table,
-//   //   Item: {
-//   //     "username": req.body.username,
-//   //     "createdAt": Date.now(),
-//   //     "thought": req.body.text
-//   //   }
-//   // };
-//   dynamodb.put(params, (err, data) => {
-//     if (err) {
-//       console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-//     } else {
-//       console.log("Added item:", JSON.stringify(data, null, 2));
-//     }
-//   });
-// });
-
-// Destroy
-router.delete('/users/:time/:username', (req, res) => {
-
-  const username = "Ray Davis"
-  const time = 1602466687289;
-  const thought = "Tolerance only for those who agree with you is no tolerance at all.";
-
-  const params = {
-    TableName: table,
-    Key: {
-      "username": username,
-      "createdAt": time,
-    },
-    KeyConditionExpression: "#ca = :time",
-    ExpressionAttributeNames: {
-      "#ca": "createdAt"
-    },
-    ExpressionAttributeValues: {
-      ":time": time,
-    }
-  }
-
-  console.log("Attempting a conditional delete...");
-  dynamodb.delete(params, (err, data) => {
-    if (err) {
-      console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
-      res.status(500).json(err); // an error occurred
-    } else {
-      console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
+      console.log('Added item:', JSON.stringify(data, null, 2));
+      res.json({ 'Added': JSON.stringify(data, null, 2) });
     }
   });
 });
 
-// // update
-// router.put('/users/:username', (req, res) => {
-//   res.json({ "which": "which" })
-// });
-  // const { time, username } = req.params;
-
-//   var table = "Movies";
-
-// var year = 2015;
-// var title = "The Big New Movie";
-
-// var params = {
-//     TableName:table,
-//     Key:{
-//         "year": year,
-//         "title": title
-//     },
-//     ConditionExpression:"info.rating <= :val",
-//     ExpressionAttributeValues: {
-//         ":val": 5.0
-//     }
-// };
 module.exports = router;
