09f452a156fca5fa39243acf275462023a822afa
Tom Chestnut <tchestnut85@gmail.com>
2021-02-28 17:24:23 -0500

    Added post functionality to the ThoughtForm component to upload an image to the S3 bucket.

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const ThoughtForm = () => {
   const [formState, setFormState] = useState({ username: '', thought: '' });
   const [characterCount, setCharacterCount] = useState(0);
+
+  const fileInput = useRef(null);
 
   // update state based on form input changes
   const handleChange = event => {
@@ -34,6 +36,34 @@ const ThoughtForm = () => {
     setCharacterCount(0);
   };
 
+  // handle the file upload form submit
+  const handleImageUpload = event => {
+    event.preventDefault();
+    const data = new FormData();
+    data.append('image', fileInput.current.files[0]);
+
+    // upload the image
+    const postImage = async () => {
+      try {
+        const res = await fetch('/api/image-upload', {
+          mode: 'cors',
+          method: 'POST',
+          body: data
+        });
+        if (!res.ok) throw new Error(res.statusText);
+        const postResponse = await res.json();
+        setFormState({ ...formState, image: postResponse.Location });
+        console.log('postImage: ', postResponse.Location);
+
+        return postResponse.Location;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    postImage();
+  };
+
+
   return (
     <div>
       <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
@@ -58,9 +88,24 @@ const ThoughtForm = () => {
           className="form-input col-12 col-md-9"
           onChange={handleChange}
         ></textarea>
+        <label className='form-input col-12 p-1'>
+          Add an image to your thought:
+          <input
+            type="file"
+            ref={fileInput}
+            className='form-input p-2'
+          />
+          <button
+            className='btn'
+            onClick={handleImageUpload}
+            type='submit'
+          >
+            Upload!
+          </button>
+        </label>
         <button className="btn col-12 col-md-3" type="submit">
           Submit
-          </button>
+        </button>
       </form>
     </div>
   );
